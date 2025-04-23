2085e3822470814c29cb40c0309f60f54c026445
oya0425 <008tessho@gmail.com>
2025-04-23 16:13:18 +0900

    端数が出ないようにする

--- recruit_quize/main.cpp
+++ recruit_quize/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 #include<string>
+#include<random>
 
 //問題文と答えをまとめる構造体
 struct Question
@@ -10,11 +11,40 @@ struct Question
 };
 
 int main() {
-	Question questions[]{
-		{"13x(-5)",13 * -5},
-		{"(-21)÷(-3)",-21 / -3},
-		{"7-(4+2)÷2",7 - (4 + 2) / 2},
-	};
+	//Question questions[]{
+	//	{"13x(-5)",13 * -5},
+	//	{"(-21)÷(-3)",-21 / -3},
+	//	{"7-(4+2)÷2",7 - (4 + 2) / 2},
+	//};
+
+	Question questions[3];
+
+	random_device rd;
+	mt19937 rand(rd());
+
+	//掛け算
+	int x = uniform_int_distribution<>(1, 30)(rand);
+	int y = uniform_int_distribution<>(1, 20)(rand);
+	questions[0].q = to_string(x) + "x" + to_string(y);
+	questions[0].a = x * y;
+
+	x = uniform_int_distribution<>(1, 30)(rand);
+	y = uniform_int_distribution<>(1, 20)(rand);
+	//questions[1].q = to_string(x) + "÷" + to_string(y);
+	//questions[1].a = x / y;
+	questions[1].q = to_string(x*y) + "÷" + to_string(y);
+	questions[1].a = x;
+
+	x = uniform_int_distribution<>(1, 100)(rand);
+	y = uniform_int_distribution<>(1, 10)(rand);
+	int z = uniform_int_distribution<>(1, 10)(rand);
+	int w = uniform_int_distribution<>(1, 10)(rand);
+	questions[2].q = to_string(x) + "-("+ to_string(y)+"+" + to_string(z)+")÷"+to_string(w);
+	questions[2].q = to_string(x) + "-(" + to_string(y*w) + "+" + to_string(z*w) + ")÷" + to_string(w);
+
+	//questions[2].a = x - (y + z) / w;
+	questions[2].a = x - (y + z);
+
 
 	cout << "[リクルート試験対策クイズ]\n";
 	//cout << "13x(-5)の答えは？\n";
