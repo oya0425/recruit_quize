83f7948edf4c8694b60958dc1601b589b7440e88
oya0425 <008tessho@gmail.com>
2025-05-28 16:24:16 +0900

    課題４

--- recruit_quize/utility.cpp
+++ recruit_quize/utility.cpp
@@ -10,6 +10,16 @@ std::vector<int> CreateRandomIndices(int n)
     }
 
     //番号の配列をシャッフル
+    Shuffle(indices);
+
+    return indices;
+}
+
+void Shuffle(vector<int>& indices) {
+
+
+    const int n = static_cast<int>(indices.size());
+    //番号の配列をシャッフル
     random_device rd;
     mt19937 rand(rd());
 
@@ -20,5 +30,24 @@ std::vector<int> CreateRandomIndices(int n)
         indices[j] = tmp;
     }
 
-    return indices;
 }
+//間違った番号の配列を作成する
+//n		作成する番号の範囲
+//correctIndex　正解の番号
+vector<int>CreateWronogIndices(int n, int correctIndex) {
+ //番号を配列に格納
+    vector<int>indices(n - 1);
+    for (int i = 0; i < correctIndex; i++) {
+        indices[i] = i;
+    }
+    for (int i = correctIndex; i < n - 1; i++) {
+        indices[i] = i + 1;
+    }
+
+    //番号の配列をシャッフル
+    Shuffle(indices);
+    return indices;
+
+
+}
+
--- recruit_quize/utility.h
+++ recruit_quize/utility.h
@@ -1,6 +1,16 @@
 #pragma once
 #include<vector>
 
+//配列をシャッフル
+void Shuffle(std::vector<int>& indices);
+
 //ランダムな番号配列を作成する
 std::vector<int>CreateRandomIndices(int n);
+
+
+//間違った番号の配列を作成する
+//n		作成する番号の範囲
+//correctIndex　正解の番号
+std::vector<int>CreateWronogIndices(int n, int correctIndex);
+
 
