2c60b79b4d9337f8bada5bcf0deceaebd77a5896
oya0425 <008tessho@gmail.com>
2025-05-28 15:46:34 +0900

    出題順をランダムにする

--- recruit_quize/exam_japanese.cpp
+++ recruit_quize/exam_japanese.cpp
@@ -1,4 +1,6 @@
 #include"exam_japanese.h"
+#include"utility.h"
+
 using namespace std;
 QuestionList CreateKanjiExam()
 {
@@ -17,8 +19,10 @@ QuestionList CreateKanjiExam()
 	constexpr int quizCount = 5;
 	QuestionList questions;
 	questions.reserve(quizCount);
+	const vector<int>indices = CreateRandomIndices(size(data));
+
 	for (int i = 0; i < quizCount; i++) {
-		const auto& e = data[i];
+		const auto& e = data[indices[i]];
 		questions.push_back({
 			"u" + string(e.kanji) + "v‚Ì“Ç‚Ý‚ð•½‰¼–¼‚Å“š‚¦‚æ",
 			e.reading });
